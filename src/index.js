1a3c4e07b62f36c65079fcdee691aeeca7ecd53a
zhixiang_w <zhixiang_w@sina.cn>
2019-04-15 16:48:06 +0800

    完善动画操作内容

--- src/index.js
+++ src/index.js
@@ -13,43 +13,43 @@ import Home from 'containers/Home'
 import Page1 from 'containers/Page1'
 import Page2 from 'containers/Page2'
 
-// 页面刷新或者appinit的时候，action为pop，所以需要一个init的stack
-const historyStack = [{ oldURL: null, newURL: window.location.href }]
-const isExist = () => historyStack.find(v => v.oldURL === hashChangeOldURL && v.newURL === window.location.href)
-let hashChangeOldURL = window.location.href
-let browserAction = 'GOBACK'
+// 一个不准确的 appHistoryStack，不对外暴露接口，不能当做历史记录参考
+const appHistoryStack = [window.location.href]
+let useAppAction = true
+let appAction = 'FORWARD'
 
 window.addEventListener('hashchange', (HashChangeEvent) => {
   const { newURL, oldURL } = HashChangeEvent
-  hashChangeOldURL = newURL
-  console.log(HashChangeEvent, historyStack)
-  // 浏览器forward按钮前进，相当于push操作，区别在于产生的action为 POP
-  if (historyStack.find(v => v.oldURL === oldURL && v.newURL === newURL)) {
-    browserAction = 'FORWARD'
+  const newURLIndex = appHistoryStack.indexOf(newURL)
+  const oldURLIndex = appHistoryStack.indexOf(oldURL)
+  if (newURLIndex === -1) {
+    appHistoryStack.push(newURL)
+  }
+  if (!useAppAction) {
+    if (newURLIndex === -1 || newURLIndex - oldURLIndex > 0) {
+      appAction = 'FORWARD'
+    } else {
+      appAction = 'GOBACK'
+    }
   } else {
-    browserAction = 'GOBACK'
+    useAppAction = true
   }
 })
 
 ReactDOM.render((
   <Provider store={store}>
     <ConnectedRouter history={localHistory}>
-      <HashRouter>
+      <HashRouter history={localHistory}>
         <Route render={({ location, history }) => {
-          let cls = history.action === 'POP' ? 'slide-right' : 'slide-left'
-          if (browserAction === 'FORWARD' && history.action === 'POP') {
-            cls = 'slide-left'
+          let animationClassName = ''
+          if (['PUSH', 'REPLACE'].includes(history.action)) {
+            useAppAction = false
+            animationClassName = 'slide-left'
+          } else {
+            animationClassName = appAction === 'FORWARD' ? 'slide-left' : 'slide-right'
           }
-          if (history.action === 'PUSH') {
-            !isExist() && historyStack.push({ oldURL: hashChangeOldURL, newURL: window.location.href })
-          }
-          if (history.action === 'REPLACE') {
-            const currentStack = historyStack.find(v => v.newURL === hashChangeOldURL)
-            currentStack.newURL = window.location.href
-          }
-          console.table(historyStack)
           return (
-            <TransitionGroup className={cls}>
+            <TransitionGroup className={animationClassName}>
               <CSSTransition key={location.pathname} classNames="animation" timeout={304}>
                 <Switch location={location}>
                   <Route exact path="/app" component={App}/>
