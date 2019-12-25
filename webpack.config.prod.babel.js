89ad596c24057176266180492aa326e8bfc708d1
zhixiang_w <zhixiang_w@sina.cn>
2019-12-25 10:43:24 +0800

    添加线上打包脚本

---
+++ webpack.config.prod.babel.js
@@ -0,0 +1,95 @@
+import path from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.[hash:16].js'
+  },
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: 'babel-loader'
+    },
+    {
+      test: /\.(css|scss)$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+      }, {
+        loader: 'style-loader'
+      },
+      {
+        loader: 'css-loader'
+      },
+      {
+        loader: 'sass-loader'
+      }
+      ]
+    },
+    {
+      test: /\.less$/,
+      use: [
+        {
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'less-loader',
+          options: {
+            javascriptEnabled: true
+          }
+        }
+      ]
+    },
+    {
+      test: /\.(gif|jpg|png|bmp|eot|woff|woff2|ttf|svg)/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          outputPath: 'images'
+        }
+      }]
+    }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      containers: path.resolve(__dirname, 'src/containers'),
+      images: path.resolve(__dirname, 'src/images'),
+      lib: path.resolve(__dirname, 'src/lib'),
+    }
+  }
+}
