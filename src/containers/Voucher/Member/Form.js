c813a5a5e06932b617f5f562869d7dd4f732ac76
zhongjinsheng <zhongjinsheng@gmail.com>
2020-12-17 18:05:04 +0800

    暂存

--- src/containers/Voucher/Member/Form.js
+++ src/containers/Voucher/Member/Form.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { creatHistory, Button, Select, Input, Form, Switch } from 'antd';
+import { creatHistory, Button, Select, Input, Form, Switch, Icon  } from 'antd';
 import { connect } from 'react-redux'
 import Action from '../action'
-import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 
-console.log(FormList,"_+_+_+_+_+_++_+_+")
 class MemberForm extends React.Component {
   constructor(props) {
     super(props)
@@ -43,22 +41,124 @@ class MemberForm extends React.Component {
 
   filterOption = (input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
 
+  remove = k => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('data');
+    // We need at least one passenger
+    if (keys.length === 1) {
+      return;
+    }
+
+    // can use data-binding to set
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  };
+
+  add = () => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('data');
+    const nextKeys = keys.concat(id++);
+    // can use data-binding to set
+    // important! notify form to detect changes
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  };
+
   render() {
     const { id } = this.props.match.params
     let { info = {}} = this.props
     if (!~~id) {
       info = {}
     }
-    
     const formItemLayout = {
-      labelCol: { span: 4 },
-      wrapperCol: { span: 8 },
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 },
+      },
     };
-    
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 20, offset: 4 },
+      },
+    };
+
     const sopts = this.props.allMemberType.map(item => (<Select.Option key={item.id} value={item.id}>{item.name}</Select.Option>))
 
-    const { getFieldDecorator } = this.props.form;
-    const fields = info.data;
+    const { getFieldDecorator, getFieldValue  } = this.props.form;
+    
+    getFieldDecorator('data', { initialValue: info.data || [] });
+    const keys = getFieldValue('data');
+    console.log(keys,"_+_+_+_+_+_+_+_+_+_+_+")
+    const formItems = keys.map((value, index) => (
+      <Form.Item
+        {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+        label={(index === 0 ? '附赠折扣券数据' : '')}
+        required={false}
+        key={index}
+      >
+        <Form.Item style={{ display: 'inline-block' }}>
+          {getFieldDecorator(`voucher[${index}][type]`, {
+            initialValue: value.type,
+            validateTrigger: ['onChange', 'onBlur'],
+            rules: [
+              {
+                required: true,
+                whitespace: true,
+                message: "请选择券类型",
+              },
+            ],
+          })(
+            <Select placeholder="选择券类型" optionFilterProp="children" filterOption={this.filterOption}>
+              <Select.Option key="1" value="1">{item.name}</Select.Option>
+            </Select>,
+          )}
+        </Form.Item>
+        <Form.Item style={{ display: 'inline-block' }}>
+          {getFieldDecorator(`voucher[${index}]['id']`, {
+            initialValue: value.id,
+            validateTrigger: ['onChange', 'onBlur'],
+            rules: [
+              {
+                required: true,
+                whitespace: true,
+                message: "请选择抵扣券",
+              },
+            ],
+          })(<Input placeholder="选择抵扣券" />)}
+        </Form.Item>
+        <Form.Item style={{ display: 'inline-block' }}>
+          {getFieldDecorator(`voucher[${index}]['num']`, {
+            initialValue: value.num,
+            validateTrigger: ['onChange', 'onBlur'],
+            rules: [
+              {
+                required: true,
+                whitespace: true,
+                message: "请填写数量",
+              },
+            ],
+          })(<Input placeholder="发放数量" />)}
+        </Form.Item>
+        <Form.Item style={{ display: 'inline-block' }}>
+          {keys.length > 1 ? (
+            <Icon
+              className="dynamic-delete-button"
+              type="minus-circle-o"
+              onClick={() => this.remove(index)}
+            />
+          ) : null}
+        </Form.Item>
+      </Form.Item>
+    ));
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <Form.Item {...formItemLayout} label="名称">
@@ -125,38 +225,12 @@ class MemberForm extends React.Component {
             initialValue: !!info.status,
           })(<Switch />)}
         </Form.Item>
-        <Form.List name="data">
-          {(fields, { add, remove }) => (
-            <>
-              {fields.map(field => (
-                <div>
-<Form.Item
-                    {...field}
-                    name={[field.name, 'first']}
-                    fieldKey={[field.fieldKey, 'first']}
-                    rules={[{ required: true, message: 'Missing first name' }]}
-                  >
-                    <Input placeholder="First Name" />
-                  </Form.Item>
-                  <Form.Item
-                    {...field}
-                    name={[field.name, 'last']}
-                    fieldKey={[field.fieldKey, 'last']}
-                    rules={[{ required: true, message: 'Missing last name' }]}
-                  >
-                    <Input placeholder="Last Name" />
-                  </Form.Item>
-                  <MinusCircleOutlined onClick={() => remove(field.name)} />
-                </div> 
-              ))}
-              <Form.Item>
-                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                  Add field
-                </Button>
-              </Form.Item>
-            </>
-          )}
-        </Form.List>
+        {formItems}
+        <Form.Item {...formItemLayoutWithOutLabel}>
+          <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
+            <Icon type="plus" /> Add field
+          </Button>
+        </Form.Item>
         <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
           <Button type="primary" htmlType="submit">
             提交
